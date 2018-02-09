af80cd19e657663f5976099dd6ce84ec7f21092f
zhengxinxin <zhengxinxin@360.net>
2018-02-09 14:58:52 +0800

    all

--- src/App.vue
+++ src/App.vue
@@ -47,6 +47,7 @@
         box-shadow: 0px 0px 5px 0px rgba(0,0,0,.3);
         margin:20px auto;
         font-family: 'Apple Lihei','Microsoft Yahei','Avenir', Helvetica, Arial, sans-serif;
+        // font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         color: #2c3e50;
--- src/components/technology.vue
+++ src/components/technology.vue
@@ -1,22 +1,39 @@
 <template>
     <jl-block title="技能特长">
         <el-row class="tech">
-            <el-col :span="18">
-                <p class="my-name">郑新新</p>
-                <p class="my-desc">我一定可以！</p>
-                <el-row >
-                    <el-col class="item" :span="12">意向岗位：Web前端工程</el-col>
-                    <el-col class="item" :span="12">意向城市：济南</el-col>
-                </el-row>
-                <el-row>
-                    <el-col class="item" :span="12">期望薪资：10k~15k</el-col>
-                    <el-col class="item" :span="12">邮箱地址：zhengxinxin1990@163.com</el-col>
-                </el-row>
-            </el-col>
-            <el-col :span="6">
-                <div class="grid-content ">
-                            
-                </div>
+            <el-col :span="24">
+                <ul>
+                    <li>
+                        <el-row >
+                            <el-col class="item" :span="24">英语六级（CET6）, 能够熟练阅读英文技术文档。</el-col>
+                        </el-row>
+                    </li>
+                    <li>
+                        <el-row>
+                            <el-col class="item" :span="24">熟练掌握前端基本技术 JavaScript (Es5 , Es6)，Html5 ，CSS3 的使用 。</el-col>
+                        </el-row>
+                    </li>
+                    <li>
+                        <el-row >
+                            <el-col class="item" :span="24">熟练掌握Vue , AngularJs , ReactJs , jQuery、Bootstrap、ReactJs、Require.js ThinkPHP框架的使用</el-col>
+                        </el-row>
+                    </li>
+                    <li>
+                        <el-row>
+                            <el-col class="item" :span="24">熟悉PHP，JavaScript，熟悉Ajax技术，熟悉MYSQL，HTML，CSS，熟悉Linux基本命令，HTML5，Git，了解ThinkJs，NodeJs</el-col>
+                        </el-row>
+                    </li>
+                    <li>
+                        <el-row >
+                            <el-col class="item" :span="24">熟悉functionchart、highchart、artdialog、easyui、Ztree插件的使用，熟悉Smarty模板，了解JSON</el-col>
+                        </el-row>
+                    </li>
+                    <li>
+                        <el-row>
+                            <el-col class="item" :span="24">熟悉PHP，JavaScript，熟悉Ajax技术，熟悉MYSQL，HTML，CSS，熟悉Linux基本命令，HTML5，Git，了解ThinkJs，NodeJs</el-col>
+                        </el-row>
+                    </li>
+                </ul>
             </el-col>
         </el-row>
     </jl-block>
@@ -35,8 +52,22 @@
 </script>
 <style scoped lang="scss"> 
     .tech{
+        li{
+            position: relative;
+            padding-left:18px;
+            &:after{
+                content:'';
+                width:8px;
+                height:8px;
+                border-radius:50%;
+                border:2px solid #264562;
+                position: absolute;
+                top:9px;
+                // transform: translateY(-50%);
+                left:0px;
+            }
+        }
         .item{
-            height:30px;
             line-height:30px;
         }
         .my-name{
